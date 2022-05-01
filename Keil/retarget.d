0181a478e0a22a911d15f60a77fd910ec84f1c0c
unknown <lukemason835@gmail.com>
2022-05-01 12:19:43 -0700

    remove conflicts

--- Keil/retarget.d
+++
@@ -1,2 +0,0 @@
-.\retarget.o: retarget.c
-.\retarget.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
--- Keil/udma.d
+++
@@ -1,4 +0,0 @@
-.\udma.o: uDMA.cpp
-.\udma.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\udma.o: uDMA.h
-.\udma.o: inc/tm4c123gh6pm.h
